b64e89eaecce80b88685cf761204ee48e2ede573
ritesh-khandekar <riteshkhandekar4003@gmail.com>
2022-08-02 13:10:41 +0530

    Attractive Navbar, Font, Donate page

--- src/website/navbar/NavExtraDark.js
+++ src/website/navbar/NavExtraDark.js
@@ -1,5 +1,6 @@
 import WEBSITE_NAME from "../tools/WebsiteName";
-
+import icon from "../images/icon_landscape.png";
+import "./Nav.css"
 const navData = [
     "Rescue",
     "Donate",
@@ -7,35 +8,44 @@ const navData = [
     "NGO Registration",
     "Shop",
     "Hospitals",
-    "",
-    "Login",
-    "Register"
+]
+const navDataRight = [
+    "LOGIN",
+    "SIGNUP"
 ]
 
-function LinkClick(){
-    
+function LinkClick() {
+
 }
 function NavLinks(props) {
     const data = props.data;
     const links = data.map((link, i) =>
-        <li className="nav-item" key={link + "i"}>
+        <li className="nav-item py-2" key={link + "i"}>
             <a className="nav-link" onClick={LinkClick}>{link}</a>
         </li>
     );
     return links;
 }
 
-function NavExtraDark(){
-    return  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container px-5">
-        <a className="navbar-brand" href="#!">{WEBSITE_NAME}</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <NavLinks data={navData}/>
-            </ul>
+function NavExtraDark() {
+    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+            <a className="navbar-brand" href="#!"><img src={icon} height="60" /></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <NavLinks data={navData} />
+                </ul>
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item py-2">
+                        <a className="nav-link btn btn-primary mx-1 text-white" onClick={LinkClick}>LOGIN</a>
+                    </li>
+                    <li className="nav-item py-2">
+                        <a className="nav-link btn btn-info mx-1 text-white" onClick={LinkClick}>SIGNUP</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 }
 export default NavExtraDark;
--- src/website/pet/Donate.js
+++ src/website/pet/Donate.js
@@ -1,4 +1,5 @@
 import "../user/RegisterForm.css"
+
 function StarRating(props) {
     const rating = props.rating;
     const val = parseInt(rating);
@@ -12,14 +13,11 @@ function StarRating(props) {
     }
     return stars;
 }
+
 function DonatePage() {
     const ngoData = [
-        { "name": "Empowering Humanity", "address": "31, Agarwal Life Style (Kavya) Global City Chikal, Road, Virar West, Dongare, Maharashtra 401303", "reviews": 1, "stars": 5, "image": "", "phone": "8293027942" },
+        { "name": "Empowering Humanity", "address": "31, Agarwal Life Style (Kavya) Global City Chikal, Road, Virar West, Dongare, Maharashtra 401303", "reviews": 1, "stars": 5, "image": "https://www.empoweringhumanity.org/wp-content/uploads/2021/09/cropped-Untitled-design-24.jpg", "phone": "8293027942" },
         { "name": "Ashwamedh Foundation", "address": "A/01, Sindhu Garden CHS. Y.K. Taluka: Vasai, District: Palghar, Pincode: 401303, Y K Nagar, Virar West, Virar, Maharashtra 401303", "reviews": 27, "stars": 4.3, "phone": "9769806731", "image": "" },
-        // { "name": "Ashwamedh Foundation", "address": "A/B 401, Sindhu Garden CHS. Y.K. Taluka: Vasai, District: Palghar, Pincode: 401303, Y K Nagar, Virar West, Virar, Maharashtra 401303", "reviews": 27, "stars": 4.3, "phone": "9769806731", "image": "" },
-        // { "name": "Ashwamedh Foundation", "address": "A/B 401, Sindhu Garden CHS. Y.K. Taluka: Vasai, District: Palghar, Pincode: 401303, Y K Nagar, Virar West, Virar, Maharashtra 401303", "reviews": 27, "stars": 4.3, "phone": "9769806731", "image": "" },
-        // { "name": "Ashwamedh Foundation", "address": "A/B 401, Sindhu Garden CHS. Y.K. Taluka: Vasai, District: Palghar, Pincode: 401303, Y K Nagar, Virar West, Virar, Maharashtra 401303", "reviews": 27, "stars": 4.3, "phone": "9769806731", "image": "" }
-
     ]
     const cards = ngoData.map((data, i) => {
         return <div className="card m-1 shadowbtn" style={{ width: 18 + "rem" }} key={data.name + "i"}>
@@ -28,12 +26,12 @@ function DonatePage() {
                 <h5 className="card-title">{data.name}</h5>
                 <p className="card-text">Rating: <StarRating rating={data.stars} /> ({data.reviews})</p>
                 {data.phone ? <p className="card-text">Contact: {data.phone}</p> : "    "}
-                <div className="text-muted card-text"><i><b></b>{data.address}</i></div>
+                <div className="text-muted card-text text-nowrap" data-toggle="tooltip" data-placement="top" title={data.address}><i><b></b>{data.address.slice(0,30)+"..."}</i></div>
                 <a className="btn btn-primary mx-auto d-block btn-lg text-white my-2">Donate</a>
             </div>
         </div>
     })
-    return <div className="d-flex p-2 flex-wrap">
+    return <div className="d-flex p-2 flex-wrap justify-content-center">
         {cards}
     </div>;
 }
